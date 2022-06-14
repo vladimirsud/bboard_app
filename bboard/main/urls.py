3213c9979426f05a485b99fa4b83927ab27b499d
vladimir <sudilovskiyv@mail.ru>
2022-06-14 12:39:26 +0300

    added pages for editing user personal data

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView, profile, BBLogoutView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView
 
 app_name = 'main'
 urlpatterns = [
-    path('account/logout',BBLogoutView.as_view(), name='logout'),
-    path('account/profile', profile, name='profile'),
-    path('account/login', BBLoginView.as_view(), name='login'),
+    path('accounts/logout', BBLogoutView.as_view(), name='logout'),
+    path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile', profile, name='profile'),
+    path('accounts/login', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name="index"),
 ]
